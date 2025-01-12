ecaf43a99ac28e0631df45479b41c2744613e930
Fajar Fathurrozak <fajarfat22@if.unjani.ac.id>
2025-01-12 15:13:35 +0700

    new file:   .vscode/launch.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/pages/berita.js 	new file:   src/pages/contact.js

---
+++ src/pages/berita.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+const Berita = () => {
+    return (
+        <>
+            <Header />
+            <div className="container md-1"></div>
+            
+            <Footer />
+        </>
+    );
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+const Contact = () => {
+    return (
+        <>
+            <Header />
+
+            <div className="container mt-5">
+                <div className="row">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Contact;
